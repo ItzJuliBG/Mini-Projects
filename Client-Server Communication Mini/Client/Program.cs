80b0bda447f00e4e2bb5634fe706baa31e08b6e0
ItzJuliBG <71836116+ItzJuliBG@users.noreply.github.com>
2024-01-17 21:15:47 +0200

    17/01/2024

---
+++ Client-Server Communication Mini/Client/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        using (var pipeClient = new NamedPipeClientStream(".", "MyPipe", PipeDirection.In))
+        {
+            Console.WriteLine("Connecting to pipe...");
+            pipeClient.Connect();
+
+            Console.WriteLine("Connected. Waiting for message...");
+            byte[] buffer = new byte[1024];
+            int bytesRead = pipeClient.Read(buffer, 0, buffer.Length);
+
+            string receivedMessage = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            Console.WriteLine($"Received message: {receivedMessage}");
+
+            Console.WriteLine("Press Enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Client-Server Communication Mini/Server/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        using (var pipeServer = new NamedPipeServerStream("MyPipe", PipeDirection.Out))
+        {
+            Console.WriteLine("Waiting for connection...");
+            pipeServer.WaitForConnection();
+
+            Console.WriteLine("Connected. Enter text to send:");
+            string message = Console.ReadLine();
+
+            byte[] buffer = Encoding.UTF8.GetBytes(message);
+            pipeServer.Write(buffer, 0, buffer.Length);
+
+            Console.WriteLine("Message sent. Press Enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
